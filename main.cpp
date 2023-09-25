9886b61cdc706a22660115c3ba678479e6f6dba6
Mahdi Chowdhury <142717756+mahdi-chy@users.noreply.github.com>
2023-09-25 23:28:50 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,70 +1,61 @@
 #include <iostream>
-#include "fstream"
 #include <vector>
-#include <string>
-#include"newtask.h"
+#include <fstream>
+#include "newtask.h"
 
-struct taStruct {
-	std::vector<std::string> tasks;
-	std::vector<std::string> taskID;
+struct Task {
+    std::string description;
+    std::string id;
 };
 
-std::string taskLoad(std::vector<taStruct>& tasks) {
-	std::vector<taStruct> result;
-	std::fstream saveFile;
-	saveFile.open("backup.txt", std::ios::in | std::ios::out | std::ios::app);
-	if (saveFile.is_open()) {
-		std::cout << "--- File Loaded ---" << std::endl;
-		std::string line;
-		std::vector<taStruct>& temp=tasks;
-		while (std::getline(saveFile,line)) {
-			result.push_back({temp.tasks, });
-			std::getline(saveFile, line);
-			tasks.push_back(line);
-		}
+std::vector<Task> tasks;
 
-	}
-	else {
-		std::cout << "file error" << std::endl;
-	}
-	saveFile.close();
+void taskLoad() {
+    // Implement loading tasks from a file (e.g., "backup.txt") into the 'tasks' vector.
+}
+
+void taskSave() {
+    // Implement saving tasks from the 'tasks' vector to a file (e.g., "backup.txt").
 }
 
 int main() {
-	bool isRunning = true;
-	taStruct tasks;
+    bool isRunning = true;
 
-	taskLoad(tasks.taskID,tasks.tasks);
-	int userInput = 0;
-	while (isRunning==true) {
-		std::cout << "---------- To-Do List ---------\n"
-			"1. Enter a new task\n"
-			"2. Delete a task\n"
-			"3. Edit a task\n"
-			"4. Show all tasks\n"
-			"5. Exit the program\n" << std::endl;
-		std::cin >> userInput;
-		std::cout<< userInput;
-		switch (userInput){
-		case 1:
-			newTask();
-			break;
-		case 2:
-			deleteTask();
-			break;
-		case 3:
-			editTask();
-			break;
-		case 4:
-			tasks=showTask(tasks.taskID,tasks.tasks);
-			break;
-		case 5:
-			isRunning = false;
-			break;
-		default:
-			break;
-		}
+    taskLoad(); // Load tasks from a file if needed
 
-	}
+    int userInput = 0;
+    while (isRunning) {
+        std::cout << "---------- To-Do List ---------\n"
+                  "1. Enter a new task\n"
+                  "2. Delete a task\n"
+                  "3. Edit a task\n"
+                  "4. Show all tasks\n"
+                  "5. Exit the program\n"
+                  << std::endl;
 
+        std::cin >> userInput;
+        switch (userInput) {
+            case 1:
+                newTask();
+                break;
+            case 2:
+                deleteTask();
+                break;
+            case 3:
+                editTask();
+                break;
+            case 4:
+                showTask();
+                break;
+            case 5:
+                isRunning = false;
+                break;
+            default:
+                break;
+        }
+    }
+
+    taskSave(); // Save tasks to a file before exiting
+
+    return 0;
 }
